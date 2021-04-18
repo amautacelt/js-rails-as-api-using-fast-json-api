f4712acd61161db0775085ebcb407b1f95c23d7f
Carlos E. La Torre <amautacelt@gmail.com>
2021-04-18 19:51:37 -0400

    commit tree bc84af25bd07f9363b3aebfda11d012177fb5edf

---
+++ app/serializers/sighting_serializer.rb
@@ -0,0 +1,12 @@
+class SightingSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :created_at
+  belongs_to :bird
+  belongs_to :location
+  
+  # attributes :created_at, :bird, :location
+  
+  # attributes :created_at
+
+  # attributes 
+end
